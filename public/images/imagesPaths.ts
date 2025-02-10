d75c1383b92afad14c65f69645fa5d7d47a7f5e2
henry-zwap <henryloth.zwap@icloud.com>
2025-02-10 10:19:57 +0800

    feat(imagePaths): add image cdn

---
+++ public/images/imagesPaths.ts
@@ -0,0 +1,15 @@
+const BASE = "gs://image-cdn-fb0d4.firebasestorage.app/jacky-profile-project/";
+const IMAGE_PATH = {
+  HUD_COVER: `${BASE}Hud_cover.jpg`,
+  COMMING_SOON_COVER: `${BASE}comming_soon_cover.jpg`,
+  CT_COVER: `${BASE}/ct_cover.jpg`,
+
+  HUD_ENSCAPE_SOCIAL: `${BASE}Hud_Enscape_social.jpg`,
+  HUD_ENSCAPE_COUNTER_PSED: `${BASE}hud_Enscape_Counter_psed.jpg`,
+  HUD_FLOOR_PLAN: `${BASE}Hud_ID_CAD_TEMPLATE_23_242-A3_COMMERCIAL_TEMPLATE_floor_plan.jpg`,
+  HUD_ENSCAPE_PRIVATE: `${BASE}hud_Enscape_Private.jpg`,
+  HUD_ID_CAD_TEMPLATE_TECHNICAL_3: `${BASE}Hud ID_CAD_TEMPLATE_23_242-A3_COMMERCIAL_TEMPLATE_technical_3.jpg`,
+  HUD_ID_CAD_TEMPLATE_TECHNICAL_4: `${BASE}Hud_ID_CAD_TEMPLATE_23_242-A3_COMMERCIAL_TEMPLATE_technical_4.jpg`,
+};
+
+export default IMAGE_PATH;
