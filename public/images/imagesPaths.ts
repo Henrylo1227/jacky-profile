a8794494da97307e902364f99c36e1b4deee20db
henry-zwap <henryloth.zwap@icloud.com>
2025-02-12 00:57:36 +0800

    feat(overview): image paths

--- public/images/imagesPaths.ts
+++ public/images/imagesPaths.ts
@@ -1,9 +1,8 @@
 const BASE = "gs://image-cdn-fb0d4.firebasestorage.app/jacky-profile-project/";
 const IMAGE_PATH = {
   HUD_COVER: `${BASE}Hud_cover.jpg`,
-  COMMING_SOON_COVER: `${BASE}comming_soon_cover.jpg`,
-  CT_COVER: `${BASE}/ct_cover.jpg`,
-
+  CT_COVER: `${BASE}comming_soon_cover.jpg`,
+  COH_COVER: `${BASE}/ct_cover.jpg`,
   HUD_ENSCAPE_SOCIAL: `${BASE}Hud_Enscape_social.jpg`,
   HUD_ENSCAPE_COUNTER_PSED: `${BASE}hud_Enscape_Counter_psed.jpg`,
   HUD_FLOOR_PLAN: `${BASE}Hud_ID_CAD_TEMPLATE_23_242-A3_COMMERCIAL_TEMPLATE_floor_plan.jpg`,
